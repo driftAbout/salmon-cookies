97e121c753185a10ff91a1c73bdd193caacbdc21
Kevin MIller <kevin@driftabout.com>
2017-10-25 01:04:16 -0700

    added comments

--- app.js
+++ app.js
@@ -58,12 +58,12 @@ function createCookiesHours(openHr, closeHr, avgCookies, thisShop){
 
     //convert partial cookies to whole cookies
     cookiesPerHour = Math.ceil(avgCookies * random_num);
-
-    cookieTotal = cookieTotal + cookiesPerHour;
+    cookieTotal += cookiesPerHour;
     console.log('hrStr', hrStr);
     console.log('avgCookies: ', avgCookies);
     console.log('thisShop.randomCustomers()', random_num);
     console.log('cookiesPerHour', cookiesPerHour);
+
     hrArr.push([hrStr, cookiesPerHour]);
 
   }
@@ -71,45 +71,34 @@ function createCookiesHours(openHr, closeHr, avgCookies, thisShop){
   //and cookie total for shop
   return [hrArr, cookieTotal];
 }
-/*
-//Helper function to build the unordered list
-function buildList(hoursArray, cTotal){
-  var listItems = [];
-  //loop through the array of hours and cookies arrays
-  for (var i = 0; i < hoursArray.length; i++){
-    //join each hours and cookies array with ': '
-    //8am: 100 Cookies
-    listItems.push(hoursArray[i].join(': ') + ' cookies');
-  }
-  //Add the total to the array
-  listItems.push('Total: ' + cTotal);
-  //convert the array into HTML for an ul with li's
-  var listItemsStr = '<ul><li>' + listItems.join('</li><li>') + '</li></ul>';
-  return listItemsStr;
-}
-*/
 
+//Helper function to build the table row
 function buildList(hoursArray, shop_location, daily_total){
   var rowItems = [];
   var headTHs = [];
   //start the array witht the shop location
   rowItems.push(shop_location);
-  //push empty string as palce holder for first header
+
+  //push empty string as palce holder for first cell in header which is empty
   headTHs.push('');
+
   //loop through the array of hours and cookies arrays
   for (var i = 0; i < hoursArray.length; i++){
     //add all hourly lotals to array
     rowItems.push(hoursArray[i][1]);
+    //add each hout to the header array
     headTHs.push(hoursArray[i][0]);
   }
-  //convert the array into HTML tr with tds
-  //var rowItemsStr = '<tr><td>' + rowItems.join('</td><td>') + '</td></tr>';
-  //add daily total quantity
-  rowItems.push(daily_total)
-  var rowItemsStr = '<td>' + rowItems.join('</td><td>') + '</td>'
-  //add daily totals cell
+
+  //add daily total quantity to the end of the array
+  rowItems.push(daily_total);
+  //convert the array into HTML string with with tds
+  var rowItemsStr = '<td>' + rowItems.join('</td><td>') + '</td>';
+  //add daily totals header label to the end of the array
   headTHs.push('Daily Totals');
+  //create the header row html string
   var headTHsStr = '<tr><th>' + headTHs.join('</th><th>') + '</th></tr>';
+
   return [rowItemsStr, headTHsStr];
 }
 
@@ -122,39 +111,59 @@ function Shop(shopLocation, min_customer_hr, max_customer_hr, avg_cookies_hr, ho
   this.avg_cookies_hr = avg_cookies_hr;
   this.hour_open = hour_open;
   this.hour_close = hour_close;
+  //array of [[hour, cookies sold], [hour, cookies sold]...]
   this.shop_hrs_cookies = [];
+  //total cookies sold in a day
   this.totalCookies = '';
+  //string of tds to be put in a tr
   this.cookieRow = '';
+  //string of html for header row <tr><td></td>,<td></td> ... </tr>
   this.hourRow = '';
+  //sales.html table id
   this.salesTableId = 'sales-table';
-  this.homeTableId = 'store-info-table';
+  //index.html table id
+  this.homeTableId = 'store_locations';
 };
 
+//create random nnumber of customers
 Shop.prototype.randomCustomers = function(){
   return randomCustomerGeneratior(this.min_customer_hr, this.max_customer_hr);
 };
 
+//create array of shop hours based on open and close times
+//along with cookies per hour and total cookies per day
 Shop.prototype.create_shop_hrs_cookies = function (){
   var cookieData = createCookiesHours(this.hour_open, this.hour_close, this.avg_cookies_hr, this);
   this.shop_hrs_cookies = cookieData[0];
   this.totalCookies = cookieData[1];
 };
 
+//create html string of table data for table row
 Shop.prototype.create_list = function(){
   var buildList_data = buildList(this.shop_hrs_cookies, this.shopLocation, this.totalCookies);
   this.cookieRow = buildList_data[0];
   this.hourRow = buildList_data[1];
 };
 
+//insert table data row on the  page
 Shop.prototype.insert_sales_row = function() {
+  //calulate the cookies per hour
   this.create_shop_hrs_cookies();
+  //create the data string for the row
   this.create_list();
+  //create the new row for the the data string
   var newRow = document.createElement('tr');
+  //add the data string to the row
   newRow.innerHTML = this.cookieRow;
+  //get a reference to the table using id
   var theTable = document.getElementById(this.salesTableId);
+  //check to see if the table head was created, if not create it
   table_head_check(theTable, this.hourRow);
+  //make sure there is a table body, if not tehn create it
   var tableBody = table_body_check(theTable);
+  //add the row to the table on the page
   tableBody.appendChild(newRow);
+  //update the foot if it exist with new totals, create if missing
   update_footer(theTable, this.shop_hrs_cookies, this.totalCookies);
 };
 
@@ -167,12 +176,10 @@ var shop_5 = new Shop('Alki', 2, 16, 4.6, '6am', '8pm');
 
 //create array of shops
 var shops = [shop_1, shop_2, shop_3, shop_4, shop_5];
-//shops[0].insert_sales_row();
-//shops[1].insert_sales_row();
+
 console.log(shops);
 
-var section;
-
+//if the script is loaded on the sales page, create the salse table
 if (document.title.toLowerCase().indexOf('sales') > 0){
 
   for (var i = 0; i < shops.length; i++){
@@ -180,61 +187,16 @@ if (document.title.toLowerCase().indexOf('sales') > 0){
     shops[i].insert_sales_row();
   }
 
-
 }
+//if the script is loaded on the index page, create the store info table
 else{
-//  buildStoreHours();
+  buildStoreHours(shops[0].homeTableId);
 }
 
-//sales-table
-function buildSalesPage(){
-  var tableRows = [];
-  var totals = ['Total'];
-  //loop through each shop
-  // call the create_shop_hrs_cookies() method to build the array of shop hours and cookies
-  // call the  create_list(); method to bulid the unordered list as a string
-  for (var i = 0; i < shops.length; i++){
-    shops[i].create_shop_hrs_cookies();
-    shops[i].create_list();
-
-    tableRows.push(shops[i].cookieRow);
-    totals.push(shop[i].totalCookies);
-
-  }
-console.log('tableRows', tableRows);
-  //build the thead and headers with the first shop hourRow data
-  var tableHead = document.createElement('thead');
-  //build the inner html
-  tableHead.innerHTML = shops[0].hourRow;
-  console.log('tableHead', tableHead);
-  var salesTable = document.getElementById('sales-table');
-  salesTable.appendChild(tableHead);
-
-  var tableBody = document.createElement('tbody');
-  tableBody.innerHTML = tableRows.join('');
-  console.log('tableBody', tableBody);
-  salesTable.appendChild(tableBody);
-
-
-
-
-/*
-  section.innerHTML = shop_location + shops[i].ul;
-  var salesData_div = document.getElementById('sales-data');
-  //  document.body.appendChild(section);
-  salesData_div.appendChild(section);
-  */
-}
-/*
-function build_table_head(headerRow, tableId) {
-  var tableHead = document.createElement('thead');
-  tableHead.innerHTML = headerRow;
-  var target_table = document.getElementById(tableId);
-  target_table.appendChild(tableHead);
-}
-*/
-
+//check the header
 function table_head_check(target_table, headerRow) {
+  // get a reference to the table head in the target table
+  // if it is missing, create it
   var tableHead = target_table.getElementsByTagName('thead');
   if (tableHead.length === 0) {
     tableHead = document.createElement('thead');
@@ -243,7 +205,10 @@ function table_head_check(target_table, headerRow) {
   }
 }
 
+//check the body
 function table_body_check(theTable) {
+  // get a reference to the table body in theTable
+  // if it is missing, create it
   var theBody = theTable.getElementsByTagName('tbody');
   if (theBody.length === 0){
     console.log('No Body');
@@ -253,6 +218,7 @@ function table_body_check(theTable) {
   return theBody[0];
 }
 
+// update the value of the totals in the footer
 function update_footer(target_table, cookies_hr_array, daily_total) {
   var hourTotals = ['Hourly Totals'];
   for (var i = 0; i < cookies_hr_array.length; i++){
@@ -262,56 +228,39 @@ function update_footer(target_table, cookies_hr_array, daily_total) {
   //add the daily total to the end
   hourTotals.push(daily_total);
   console.log('hourTotals:', hourTotals);
+  // get a reference to the table footer in the target table
+  // if it is missing, create it
   var theFoot = target_table.getElementsByTagName('tfoot');
   if (theFoot.length === 0 ){
     var newFoot = document.createElement('tfoot');
     var rowString = '<tr><td>' + hourTotals.join('</td><td>') + '</td></tr>';
-    console.log('rowString: ',  rowString)
+    console.log('rowString: ', rowString);
     newFoot.innerHTML = rowString;
     theFoot = target_table.appendChild(newFoot);
     return;
   }
-  var newTotals = ['Hourly Totals'];
+  // if the foot exists update teh totals
+  // loop through the tds, except for the labe, whcih is first
+  //and grb the current value then add the hourly total for the row just added
   var footerTds = theFoot[0].getElementsByTagName('td');
-  console.log('footerTds', footerTds)
+  console.log('footerTds', footerTds);
   for (var j = 1; j < footerTds.length; j++){
-    console.log('footerTds[j]: ',  footerTds[j]);
+    console.log('footerTds[j]: ', footerTds[j]);
+    //get the current value then parse into a number
     var textVal = footerTds[j].textContent;
     console.log('textVal: ', textVal);
     var currentValue = parseInt(textVal);
     var newValue = currentValue + hourTotals[j];
-    newTotals.push(newValue);
-  }
-  console.log('newTotals: ', newTotals)
-  rowString = '<tr><td>' + newTotals.join('</td><td>') + '</td></tr>';
-  theFoot[0].innerHTML = rowString;
-  //target_table.appendChild(theFoot[0]);
-}
+    //update the current td
+    footerTds[j].textContent = newValue;
 
-
-/*
-function buildSalesPage(){
-  //loop through each shop
-  // call the create_shop_hrs_cookies() method to build the array of shop hours and cookies
-  // call the  create_list(); method to bulid the unordered list as a string
-  for (var i = 0; i < shops.length; i++){
-    shops[i].create_shop_hrs_cookies();
-    shops[i].create_list();
-    //create a new section
-    section = document.createElement('section');
-    //build the inner html
-    var shop_location_tag = 'p';
-    var shop_location = '<' + shop_location_tag + '>' + shops[i].shopLocation + '</' + shop_location_tag + '>';
-    //insert the html and add it to the document
-    section.innerHTML = shop_location + shops[i].ul;
-    var salesData_div = document.getElementById('sales-data');
-    //  document.body.appendChild(section);
-    salesData_div.appendChild(section);
   }
 }
-*/
 
-function buildStoreHours(){
+////////////////////////////
+//still using on index.html
+// create store hours for the index page
+function buildStoreHours(store_locations_id){
   var locations = [];
   for (var i = 0; i < shops.length; i++){
     locations.push('<span>' + shops[i].shopLocation + '</span><span class="open-hour">' + shops[i].hour_open + '</span><span>' + shops[i].hour_close + '</span>');
@@ -322,10 +271,12 @@ function buildStoreHours(){
   console.log('ulEl:', ulEl);
   var storeP = document.createElement('p');
   storeP.innerHTML = '<span>Location</span><span>Open</span><span>Close</span>';
-  var store_locations = document.getElementById('store_locations');
+  var store_locations = document.getElementById(store_locations_id);
   store_locations.appendChild(storeP);
   store_locations.appendChild(ulEl);
 }
+/////////////////////////////////
+//////////////////////////////////
 
 console.log('shops: ',shops);
 console.log('window.location: ', window.location);
