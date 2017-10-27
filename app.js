ece112f19ec047f1162af8b2f6cd4efb8c936662
Kevin MIller <kevin@driftabout.com>
2017-10-26 23:05:01 -0700

    working on edit

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var new_tHead;
 var new_tBody;
 var new_table_row;
 var shops = [];
+var submitText;
+var currentShops = [];
 
 //Create Shop Object constructor
 function Shop(shopLocation, min_customer_hr, max_customer_hr, avg_cookies_hr, hour_open, hour_close) {
@@ -16,10 +18,11 @@ function Shop(shopLocation, min_customer_hr, max_customer_hr, avg_cookies_hr, ho
   this.avg_cookies_hr = avg_cookies_hr;
   this.shop_hours = {open: hour_open, close: hour_close};
   //array of each hour open
-  this.shop_hours_array = [];
+  //this.shop_hours_array = [];
+  this.shop_hours_array = this.hours_array(this.shop_hours.open, this.shop_hours.close);
   this.standard_hours = {open:'6am', close:'8pm'};
   //array of standard hours
-  this.standard_hours_array = [];
+  this.standard_hours_array = this.hours_array(this.standard_hours.open, this.standard_hours.close);
   //array of [[hour, cookies sold], [hour, cookies sold]...]
   this.shop_hrs_cookies = [];
   //total cookies sold in a day
@@ -28,36 +31,48 @@ function Shop(shopLocation, min_customer_hr, max_customer_hr, avg_cookies_hr, ho
   this.cookie_td_str = '';
   //string of html for header row <tr><td></td>,<td></td> ... </tr>
   this.headerRow = '';
+  this.calc_cookies_per_hr();
+  this.create_row_data();
 };
 
 //create random nnumber of customers
 Shop.prototype.randomCustomers = function(){
-    var min_num = this.min_customer_hr;
-    var max_num = this.max_customer_hr;
-    return Math.floor((Math.random() * (max_num - min_num)) + min_num);
+  var min_num = this.min_customer_hr;
+  var max_num = this.max_customer_hr;
+  return Math.floor((Math.random() * (max_num - min_num)) + min_num);
 };
 
-//populate the properties for use in tabel creation
-Shop.prototype.create_store_hours_data = function(){
-  //create array of hours open based on open and closing times
-  this.shop_hours_array = hours_array(this.shop_hours.open, this.shop_hours.close);
-  //build array of possible hours open (standard hours open)
-  this.standard_hours_array = hours_array(this.standard_hours.open, this.standard_hours.close);
-  //calculate cookiws per sold hour open
-  this.calc_cookies_per_hr();
-  //build data string of tds for table
-  this.create_row_data();
+Shop.prototype.hours_array = function(openHr, closeHr){
+  //convert opening and closing times to 24hr clock numbers
+  // use this as counter start and stop
+  var startHr = time24(openHr);
+  var stopHr = time24(closeHr);
+
+  //array to hold data to return to object method
+  var hrArr = [];
+  var amPm = 'am';
+  var hrStr;
+
+  for (var i = startHr; i <= stopHr; i++){
+    var hr12Num = i;
+    //convert i into 12 clock number
+    //13 = 1
+    if (i >= 12){
+      amPm = 'pm';
+      if (i != 12){
+        hr12Num = i - 12;
+      }
+    }
+    //add am or pm to hour
+    hrStr = hr12Num.toString() + amPm;
+    //console.log('hrStr: ',  hrStr);
+    hrArr.push(hrStr);
+  }
+  return hrArr;
 };
 
 //create array of shop hours based on open and close times
 //along with cookies per hour and total cookies per day
-Shop.prototype.calc_cookies_per_hr = function (){
-  //var cookieData = createCookiesHours(this.shop_hours_array, this.standard_hours_array, this.avg_cookies_hr, this);
-  //this.shop_hrs_cookies = cookieData[0];
-  //this.totalCookies = cookieData[1];
-};
-
-//Shop.prototype.createCookiesHours = function(){
 Shop.prototype.calc_cookies_per_hr = function (){
   var shopHours = this.shop_hours_array;
   var standardHours = this.standard_hours_array;
@@ -86,7 +101,8 @@ Shop.prototype.calc_cookies_per_hr = function (){
       cookiesPerHour = 'closed';
     }
     //  console.log('[standardHours[i], cookiesPerHour]', standardHours[i], cookiesPerHour);
-    this.shop_hrs_cookies.push([standardHours[i], cookiesPerHour]);
+    //this.shop_hrs_cookies.push([standardHours[i], cookiesPerHour]);
+    this.shop_hrs_cookies.push({hour: standardHours[i], cookies: cookiesPerHour});
   }
 };
 
@@ -106,7 +122,7 @@ Shop.prototype.create_row_data = function(){
   //loop through the array of hours and cookies arrays
   for (var i = 0; i < stand_HrsArr.length; i++){
     //add all hourly lotals to array
-    rowItems.push(hoursArray[i][1]);
+    rowItems.push(hoursArray[i].cookies);
     //add each hout to the header array
     headTHs.push(stand_HrsArr[i]);
   }
@@ -158,14 +174,12 @@ function get_form_data(event) {
   admin_form.reset();
   //change cancel button text to say close after adding a row
   cancel_btn.textContent = 'Close';
-
 }
 
 //bind the form submitt button
 admin_form.addEventListener('submit', get_form_data);
 
 function build_sales_table(){
-  create_all_store_data();
   //if the script is loaded on the index page, create the store info table
   if (document.title.toLowerCase().indexOf('sales') === -1){
     buildStoreHours(locations_table_ref);
@@ -178,14 +192,6 @@ function build_sales_table(){
   }
 }
 
-
-function create_all_store_data(){
-  for (var i = 0; i < shops.length; i++){
-    //call  method of shops
-    shops[i].create_store_hours_data();
-  }
-}
-
 function build_table_header(){
   new_tHead = document.createElement('thead');
   //add the headerRow property from any of the shops as the header row
@@ -194,15 +200,6 @@ function build_table_header(){
 }
 
 function build_table_body(){
-  /*
-  new_tBody = sales_table_ref.getElementsByTagName('tbody');
-
-  if (! new_tBody.length){
-
-  } else {
-    new_tBody = new_tBody[0];
-  }
-  */
   new_tBody = document.createElement('tbody');
   for (var i = 0; i < shops.length; i++){
     new_table_row = document.createElement('tr');
@@ -223,47 +220,41 @@ function build_table_footer(){
 function get_column_totals(){
   //array to hold hourly totals, start with a label name
   var hourly_totals = ['Hourly Totals'];
-  var tr_tds;
-  var td_value;
+  var shop_cookies;
+  var shop_cookie_hr_val;
   var columnTotal;
-  //get all the rows from the table body
-  var sales_table_rows = new_tBody.getElementsByTagName('tr');
-  //get count of columns in the table
-  var column_count = sales_table_rows[0].getElementsByTagName('td').length;
-  //start at the second colum since the first is locations
+
+  //get count of items in the array of hours/cookies
+  var column_count = shops[0].shop_hrs_cookies.length;
   // loop through each row grabbing the colum by column index
-  for (var column = 1; column < column_count; column++){
-    // running total for each column
+  for (var column = 0; column < column_count; column++){
     columnTotal = 0;
-    for (var i = 0; i < sales_table_rows.length; i++){
-      //get array of tds from the row
-      tr_tds = sales_table_rows[i].getElementsByTagName('td');
-      //get value of td at column index  and parse as number
-      td_value = parseInt(tr_tds[column].textContent);
+    for (var i = 0; i < shops.length; i++){
+      shop_cookies = shops[i].shop_hrs_cookies;
+      //get the number of cookies at the column index and parse as number
+      shop_cookie_hr_val = parseInt(shop_cookies[column].cookies);
       //if the value is NaN the set the value to zero because the store is closed at that hour
-      if (isNaN(td_value)) {
-        td_value = 0;
+      if (isNaN(shop_cookie_hr_val)) {
+        shop_cookie_hr_val = 0;
       }
       //aggregate the total
-      columnTotal += td_value;
+      columnTotal += shop_cookie_hr_val;
     }
     //push the column total to the array
     hourly_totals.push(columnTotal);
   }
+  //add daily totals
+  columnTotal = 0;
+  for (var i = 0; i < shops.length; i++){
+    columnTotal += shops[i].totalCookies;
+  }
+  hourly_totals.push(columnTotal);
+
   //console.log('hourly_totals', hourly_totals);
   return hourly_totals;
 }
 
 function update_footer(hourTotals){
-  /*
-  var theFoot = sales_table_ref.getElementsByTagName('tfoot');
-  if (theFoot.length === 0 ){
-    var newFoot = document.createElement('tfoot');
-    theFoot = sales_table_ref.appendChild(newFoot);
-  } else {
-    theFoot = theFoot[0];
-  }
-  */
   var newFoot = document.createElement('tfoot');
   var theFoot = sales_table_ref.appendChild(newFoot);
   //create the string for the hourly totals in the footer
@@ -287,43 +278,6 @@ function time24(convert_hr_str){
   return hrNum;
 }
 
-//function to generate a random number of customers based on the min amd max values
-function randomCustomerGeneratior(min_num, max_num){
-  return Math.floor((Math.random() * (max_num - min_num)) + min_num);
-}
-
-
-
-function hours_array(openHr, closeHr){
-  console.log('openHr: ', openHr, 'closeHr: ', closeHr);
-  //convert opening and closing times to 24hr clock numbers
-  // use this as counter start and stop
-  var startHr = time24(openHr);
-  var stopHr = time24(closeHr);
-
-  //array to hold data to return to object method
-  var hrArr = [];
-  var amPm = 'am';
-  var hrStr;
-
-  for (var i = startHr; i <= stopHr; i++){
-    var hr12Num = i;
-    //convert i into 12 clock number
-    //13 = 1
-    if (i >= 12){
-      amPm = 'pm';
-      if (i != 12){
-        hr12Num = i - 12;
-      }
-    }
-    //add am or pm to hour
-    hrStr = hr12Num.toString() + amPm;
-    //console.log('hrStr: ',  hrStr);
-    hrArr.push(hrStr);
-  }
-  return hrArr;
-}
-
 //button to close the add new store form
 var cancel_btn = document.getElementById('cancel_btn');
 //bind click to cancel button
@@ -338,16 +292,123 @@ open_editor_btn.addEventListener('click', openEditor);
 function closeEditor(event) {
   event.preventDefault();
   var edit_window = document.getElementById('table_editor');
-  edit_window.style.display = 'none';
+  edit_window.classList.remove('active');
+  //edit_window.style.display = 'none';
   document.getElementsByClassName('sales-data')[0].style.opacity = 1;
 }
 //function to open add new store window
 function openEditor(event) {
+  console.log('open editoe')
+  get_shop_edit_data('edit_store');
   event.preventDefault();
   var edit_window = document.getElementById('table_editor');
   document.getElementsByClassName('sales-data')[0].style.opacity = .5;
-  edit_window.style.display = 'block';
+  edit_window.classList.add('active');
+  //edit_window.style.display = 'block';
 }
+
+//button to close the add new store form
+var left_tab_btn = document.getElementById('left_tab');
+var right_tab_btn = document.getElementById('right_tab');
+//bind click to cancel button
+left_tab_btn.addEventListener('click', toggle_editor_tabs);
+right_tab_btn.addEventListener('click', toggle_editor_tabs);
+
+function toggle_editor_tabs(e){
+  if(! e.target.className){
+    e.target.parentElement.getElementsByClassName('active')[0].classList.remove('active');
+    e.target.className = 'active';
+  }
+
+  if (e.target.id === 'right_tab'){
+    submitText = document.getElementById('submit_btn').textContent;
+    console.log('submitText', submitText)
+    //css reveals dropdown
+    document.getElementById('submit_btn').textContent = 'Save Changes'
+    document.getElementById('edit_store').classList.add('active');
+
+  } else {
+    document.getElementById('edit_store').classList.remove('active');
+    document.getElementById('submit_btn').textContent = submitText
+  }
+}
+
+var edit_store_ul = document.getElementById('edit_store');
+edit_store_ul.addEventListener('click', toggle_edit_li);
+
+function toggle_edit_li(e){
+  console.log('e.target', e.target)
+  console.log('lis: ', e.target.parentNode.getElementsByTagName('li').length);
+  e.target.parentNode.classList.toggle('show_li');
+  console.log('clicked li text', e.target.innerHTML);
+  console.log('first li: ', e.target.parentNode.getElementsByTagName('li')[0].innerHTML)
+  var li_arr = e.target.parentNode.getElementsByTagName('li')
+  for (var indx = 0; indx < li_arr.length; indx++){
+    console.log('This Li', li_arr[indx].innerHTML)
+    if(e.target.innerHTML === li_arr[indx].innerHTML){
+      var li_index = indx - 1;
+    }
+  }
+console.log('li_index', li_index);
+  console.log('clicked Li:' , li_index);
+  if (e.target.innerHTML != li_arr[0].innerHTML){
+    //currentShops[li_index]
+    var store_input = document.getElementsByName('store_location');
+    var open_input = document.getElementsByName('open');
+    open_input[0].value = currentShops[li_index].open;
+    var close_input = document.getElementsByName('close')
+    close_input[0].value = currentShops[li_index].close;
+    var min_customers_input = document.getElementsByName('min_customers');
+    min_customers_input[0].value = currentShops[li_index].min_customer_hr;
+    var max_customers_input = document.getElementsByName('max_customers')
+    max_customers_input[0].value = currentShops[li_index].max_customer_hr;
+    var average_cookies_input = document.getElementsByName('average_cookies')
+    average_cookies_input[0].value = currentShops[li_index].avg_cookies_hr;
+    console.log('store_input', store_input[0]);
+    store_input[0].value = currentShops[li_index].shopLocation;
+    //shops.remove(li_index); 
+    e.target.parentNode.classList.toggle('show_li');
+    e.target.parentNode.classList.toggle('active')
+  }
+
+  //var choices = e.target.parentNode.getElementsByTagName('li');
+  //for (var c = 0; c < choices;  c++){
+  //  choices[c].classList.add('show_li');
+//  }
+
+  //e.target.querySelectorAll('li').classList.add('active');
+}
+
+function get_shop_edit_data(ul_id){
+  console.log('shops here', shops)
+  for (var i = 0; i < shops.length; i++){
+    console.log('store_location: ', shops[i].shopLocation)
+    currentShops.push(new  Shop_form_data(shops[i], ul_id));
+  }
+}
+
+function Shop_form_data(this_shop, ul_id){
+  this.shopLocation = this_shop.shopLocation
+  this.min_customer_hr = this_shop.min_customer_hr;
+  this.max_customer_hr = this_shop.max_customer_hr;
+  this.avg_cookies_hr = this_shop.avg_cookies_hr;
+  this.open = this_shop.shop_hours.open;
+  this.close = this_shop.shop_hours.close;
+  this.ul_id = ul_id
+  this.makeli();
+  this.append_li();
+}
+Shop_form_data.prototype.makeli = function(){
+  var newLi = document.createElement('li');
+  newLi.innerHTML = this.shopLocation;
+  this.li_tag = newLi;
+};
+
+Shop_form_data.prototype.append_li = function(){
+  document.getElementById(this.ul_id).appendChild(this.li_tag);
+}
+
+
 
 ////////////////////////////
 //still using on index.html
